085135f87777208b65bca2d6bb8b841e7711e9cc
Kent <kentup777@gmail.com>
2024-01-14 20:25:18 -0800

    Counter: Update Increment and Decrement Functions
    	modified:   src/instructions.rs
    	modified:   src/lib.rs

--- Counter/src/lib.rs
+++ Counter/src/lib.rs
@@ -34,11 +34,15 @@ pub fn process_instruction(
     let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
 
     match instruction {
-        CounterInstructions::Increment => {
-            counter_account.counter += 1;
+        CounterInstructions::Increment(args) => {
+            counter_account.counter += args.value;
         }
-        CounterInstructions::Decrement => {
-            counter_account.counter -= 1;
+        CounterInstructions::Decrement(args) => {
+            if counter_account.counter > args.value {
+                counter_account.counter -= args.value;
+            } else {
+                counter_account.counter = 0;
+            }
         }
         CounterInstructions::Reset => {
             counter_account.counter = 0;
@@ -79,12 +83,25 @@ mod tests {
 
         let accounts = vec![account];
 
-        let increment_instruction_data: Vec<u8> = vec![0];
-        let decrement_instruction_data: Vec<u8> = vec![1];
+        let mut increment_instruction_data: Vec<u8> = vec![0];
+        let mut decrement_instruction_data: Vec<u8> = vec![1];
         let mut update_instruction_data: Vec<u8> = vec![2];
         let reset_instruction_data: Vec<u8> = vec![3];
 
+        let increment_value = 2u32;
+        increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
         process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
+
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            2
+        );
+
+        let mut decrement_value = 1u32;
+        decrement_instruction_data.extend_from_slice(&decrement_value.to_le_bytes());
+        process_instruction(&program_id, &accounts, &decrement_instruction_data).unwrap();
 
         assert_eq!(
             CounterAccount::try_from_slice(&accounts[0].data.borrow())
@@ -93,6 +110,9 @@ mod tests {
             1
         );
 
+        decrement_value = 2u32;
+        decrement_instruction_data = vec![1];
+        decrement_instruction_data.extend_from_slice(&decrement_value.to_le_bytes());
         process_instruction(&program_id, &accounts, &decrement_instruction_data).unwrap();
 
         assert_eq!(
